2ec4926413be5bf9436e4c51f471901856cb1652
Rizul <rizulgoyal23@gmail.com>
2019-10-19 18:27:31 -0400

    new class

---
+++ C0766598_F2019_MID_TERM_MAD3004/Customer.swift
@@ -0,0 +1,14 @@
+//
+//  Customer.swift
+//  C0766598_F2019_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2019-10-19.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class Customer : Bill
+{
+    
+}
